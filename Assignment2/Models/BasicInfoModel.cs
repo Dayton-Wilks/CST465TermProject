3c19c43452ac4880a5443874fe0eff40115ee8d3
Dayton-Wilks <d.wilks.trash@gmail.com>
2018-10-17 23:39:13 -0700

    Assignment 2 done

---
+++ Assignment2/Models/BasicInfoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class BasicInfoModel
+    {
+        [Required(ErrorMessage = "You need to enter a name!")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You need to enter a phone number!")]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "You need to enter an email!")]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Message")]
+        public string Message { get; set; }
+    }
+}
